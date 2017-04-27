754ab926d5a9f6faecc1cc205960f732abe108c3
Matt Diephouse <matt@diephouse.com>
2017-04-26 21:58:45 -0400

    Clean up fixtures

--- Tests/Fixtures.swift
+++ Tests/Fixtures.swift
@@ -1,14 +1,38 @@
 import Foundation
 import Schemata
 
-private struct Book {
+// MARK: - Book
+
+struct Book {
     struct ID {
         let string: String
+        
+        init(_ string: String) {
+            self.string = string
+        }
     }
     
     let id: ID
     var title: String
     var author: Author
+}
+
+extension Book.ID: Equatable {
+    static func == (lhs: Book.ID, rhs: Book.ID) -> Bool {
+        return lhs.string == rhs.string
+    }
+}
+
+extension Book: Equatable {
+    static func == (lhs: Book, rhs: Book) -> Bool {
+        return lhs.id == rhs.id && lhs.title == rhs.title && lhs.author == rhs.author
+    }
+}
+
+extension Book.ID: KeyPathCompliant {
+    func value<Leaf>(of keyPath: KeyPath<Book.ID, Leaf>) -> Leaf {
+        fatalError()
+    }
 }
 
 extension KeyPath where Root == Book {
@@ -17,11 +41,58 @@ extension KeyPath where Root == Book {
     var title: KeyPath<Root, String> { return KeyPath<Root, String>(keys: keys + ["title"]) }
 }
 
-extension Book {
+extension Book: KeyPathCompliant {
     private static let root = KeyPath<Book, Book>(keys: [])
     static let id = root.id
     static let author = root.author
     static let title = root.title
+    
+    func value<Leaf>(of keyPath: KeyPath<Book, Leaf>) -> Leaf {
+        switch keyPath.keys.first {
+        case "id"?:
+            return id as! Leaf
+        case "title"?:
+            return title as! Leaf
+        case "author"?:
+            let rest = Array(keyPath.keys.dropFirst())
+            return author.value(of: KeyPath<Author, Leaf>(keys: rest))
+        default:
+            fatalError()
+        }
+    }
+}
+
+// MARK: - Author
+
+struct Author {
+    struct ID {
+        let string: String
+        
+        init(_ string: String) {
+            self.string = string
+        }
+    }
+    
+    let id: ID
+    var name: String
+}
+
+extension Author.ID: Equatable {
+    static func == (lhs: Author.ID, rhs: Author.ID) -> Bool {
+        return lhs.string == rhs.string
+    }
+}
+
+extension Author: Equatable {
+    static func == (lhs: Author, rhs: Author) -> Bool {
+        return lhs.id == rhs.id && lhs.name == rhs.name
+    }
+}
+
+extension Author.ID: KeyPathCompliant {
+    func value<Leaf>(of keyPath: KeyPath<Author.ID, Leaf>) -> Leaf {
+        fatalError()
+    }
 }
 
 extension KeyPath where Root == Author {
@@ -43,30 +114,5 @@ extension Author: KeyPathCompliant {
         default:
             fatalError()
         }
-    }
-}
-
-struct Author: Equatable {
-    struct ID: Equatable {
-        let string: String
-        
-        init(_ string: String) {
-            self.string = string
-        }
-        
-        static func ==(_ lhs: ID, _ rhs: ID) -> Bool {
-            return lhs.string == rhs.string
-        }
-        
-        func value<Leaf>(of keyPath: KeyPath<Author.ID, Leaf>) -> Leaf {
-            fatalError()
-        }
-    }
-    
-    let id: ID
-    var name: String
-    
-    static func ==(_ lhs: Author, _ rhs: Author) -> Bool {
-        return lhs.id == rhs.id && lhs.name == rhs.name
     }
 }
