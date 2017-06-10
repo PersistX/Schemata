7adda21a32d561ddb10f57a8c7974e331eaaae7d
Matt Diephouse <matt@diephouse.com>
2017-06-09 19:43:48 -0700

    Add some more tests

--- Tests/SchemaTests.swift
+++ Tests/SchemaTests.swift
@@ -2,12 +2,30 @@ import Schemata
 import XCTest
 
 class SchemaTests: XCTestCase {
-    func test_properties_string() {
+    // MARK: subscript(_: KeyPath)
+    
+    func test_subscript_string() {
         let title = Book.schema[\.title]
         XCTAssertEqual(title.keyPath, \Book.title)
         XCTAssertEqual(title.path, "title")
         XCTAssertEqual(title.type, .value(String.self))
     }
+    
+    func test_subscript_toOne_string() {
+        let author = Book.schema[\.author]
+        XCTAssertEqual(author.keyPath, \Book.author)
+        XCTAssertEqual(author.path, "author")
+        XCTAssertEqual(author.type, .toOne(Author.self))
+    }
+    
+    func test_subscript_toMany_string() {
+        let books = Author.schema[\.books]
+        XCTAssertEqual(books.keyPath, \Author.books)
+        XCTAssertEqual(books.path, "(Book)")
+        XCTAssertEqual(books.type, .toMany(Book.self))
+    }
+    
+    // MARK: - properties(for: KeyPath)
     
     func test_propertiesForKeyPath_string() {
         let properties = Book.schema.properties(for: \.title)
@@ -19,6 +37,13 @@ class SchemaTests: XCTestCase {
         XCTAssertEqual(properties, [
             Book.schema[\.author],
             Author.schema[\.name],
+        ])
+    }
+    
+    func test_propertiesForKeyPath_toMany() {
+        let properties = Author.schema.properties(for: \.books)
+        XCTAssertEqual(properties, [
+            Author.schema[\.books],
         ])
     }
 }
