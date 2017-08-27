e5fce0235b59f24e50349b733effa401fabd4d00
Matt Diephouse <matt@diephouse.com>
2017-08-27 16:03:47 -0400

    Add Projection.keyPaths

--- Sources/Projection.swift
+++ Sources/Projection.swift
@@ -1,9 +1,16 @@
 import Foundation
 
 public struct Projection<Model: Schemata.Model, Value> {
+    /// The `KeyPath`s that are required to create a `Value`.
+    public let keyPaths: Set<PartialKeyPath<Model>>
+    
     fileprivate let make: ([PartialKeyPath<Model>: Any]) -> Value
     
-    fileprivate init(make: @escaping ([PartialKeyPath<Model>: Any]) -> Value) {
+    fileprivate init(
+        _ keyPaths: Set<PartialKeyPath<Model>>,
+        make: @escaping ([PartialKeyPath<Model>: Any]) -> Value
+    ) {
+        self.keyPaths = keyPaths
         self.make = make
     }
     
@@ -18,7 +25,7 @@ extension Projection {
         _ a: KeyPath<Model, A>,
         _ b: KeyPath<Model, B>
     ) {
-        self.init { values in
+        self.init([a, b]) { values in
             return f(
                 values[a] as! A,
                 values[b] as! B
--- Tests/ProjectionTests.swift
+++ Tests/ProjectionTests.swift
@@ -2,6 +2,14 @@ import Schemata
 import XCTest
 
 class ProjectionTests: XCTestCase {
+    func test_keyPaths() {
+        let projection = BookViewModel.projection
+        XCTAssertEqual(projection.keyPaths, [
+            \Book.title,
+            \Book.author.name,
+        ])
+    }
+    
     func test_makeValue() {
         let viewModel = BookViewModel(
             title: "The Martian Chronicles",
