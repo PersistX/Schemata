98d076764352f70ece990b1788846c7174a90b29
Matt Diephouse <matt@diephouse.com>
2017-08-27 16:17:34 -0400

    Add more Projection.inits

--- Sources/Projection.swift
+++ Sources/Projection.swift
@@ -24,14 +24,86 @@ public struct Projection<Model: Schemata.Model, Value> {
 
 extension Projection {
     public init<A, B>(
-        _ f: @escaping (A, B) -> Value,
+        _ make: @escaping (A, B) -> Value,
         _ a: KeyPath<Model, A>,
         _ b: KeyPath<Model, B>
     ) {
         self.init([a, b]) { values in
-            return f(
+            return make(
                 values[a] as! A,
                 values[b] as! B
+            )
+        }
+    }
+    
+    public init<A, B, C>(
+        _ make: @escaping (A, B, C) -> Value,
+        _ a: KeyPath<Model, A>,
+        _ b: KeyPath<Model, B>,
+        _ c: KeyPath<Model, C>
+    ) {
+        self.init([a, b, c]) { values in
+            return make(
+                values[a] as! A,
+                values[b] as! B,
+                values[c] as! C
+            )
+        }
+    }
+    
+    public init<A, B, C, D>(
+        _ make: @escaping (A, B, C, D) -> Value,
+        _ a: KeyPath<Model, A>,
+        _ b: KeyPath<Model, B>,
+        _ c: KeyPath<Model, C>,
+        _ d: KeyPath<Model, D>
+    ) {
+        self.init([a, b, c, d]) { values in
+            return make(
+                values[a] as! A,
+                values[b] as! B,
+                values[c] as! C,
+                values[d] as! D
+            )
+        }
+    }
+    
+    public init<A, B, C, D, E>(
+        _ make: @escaping (A, B, C, D, E) -> Value,
+        _ a: KeyPath<Model, A>,
+        _ b: KeyPath<Model, B>,
+        _ c: KeyPath<Model, C>,
+        _ d: KeyPath<Model, D>,
+        _ e: KeyPath<Model, E>
+    ) {
+        self.init([a, b, c, d, e]) { values in
+            return make(
+                values[a] as! A,
+                values[b] as! B,
+                values[c] as! C,
+                values[d] as! D,
+                values[e] as! E
+            )
+        }
+    }
+    
+    public init<A, B, C, D, E, F>(
+        _ make: @escaping (A, B, C, D, E, F) -> Value,
+        _ a: KeyPath<Model, A>,
+        _ b: KeyPath<Model, B>,
+        _ c: KeyPath<Model, C>,
+        _ d: KeyPath<Model, D>,
+        _ e: KeyPath<Model, E>,
+        _ f: KeyPath<Model, F>
+    ) {
+        self.init([a, b, c, d, e, f]) { values in
+            return make(
+                values[a] as! A,
+                values[b] as! B,
+                values[c] as! C,
+                values[d] as! D,
+                values[e] as! E,
+                values[e] as! F
             )
         }
     }
