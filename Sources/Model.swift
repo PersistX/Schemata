3423b4e98a4cc8d72e1d9a5bf781eaaad6971ba7
Matt Diephouse <matt@diephouse.com>
2018-07-14 13:45:44 -0400

    Move Optional's Hashable conformance inside a version check

--- Sources/Model.swift
+++ Sources/Model.swift
@@ -71,7 +71,20 @@ extension Int: ModelValue {
     public static let value = Value<Int, Int>()
 }
 
-extension Optional: Hashable, AnyModelValue, ModelValue where Wrapped: ModelValue {
+#if !swift(>=4.2)
+    extension Optional: Hashable where Wrapped: ModelValue {
+        public var hashValue: Int {
+            switch self {
+            case .none:
+                return 0
+            case let .some(value):
+                return value.hashValue
+            }
+        }
+    }
+#endif
+
+extension Optional: AnyModelValue, ModelValue where Wrapped: ModelValue {
     public typealias Encoded = Wrapped.Encoded?
 
     public static var value: Value<Wrapped.Encoded?, Wrapped?> {
@@ -106,15 +119,6 @@ extension Optional: Hashable, AnyModelValue, ModelValue where Wrapped: ModelValu
                     .map { Optional($0 as! Wrapped) as Any }
             }
         )
-    }
-
-    public var hashValue: Int {
-        switch self {
-        case .none:
-            return 0
-        case let .some(value):
-            return value.hashValue
-        }
     }
 }
 
